8c4bd85e95164e7c5ab6de9355aaa9b9cba3cb74
noopurkharche <32528824+noopurkharche@users.noreply.github.com>
2019-05-17 16:46:04 -0500

    Create trappingRainWater.py

---
+++ trappingRainWater.py
@@ -0,0 +1,23 @@
+H = [0,1,0,2,1,0,1,3,2,1,2,1]
+n = len(H)
+maxl = 0
+maxr = 0
+left = 0
+right = n-1
+water = 0
+
+while left < right:
+    if H[left] < H[right]:
+        if H[left] >= maxl:
+            maxl = H[left]
+        else:
+            water = water + ( maxl - H[left])
+        left = left + 1 
+    else:
+        if H[right] >= maxr:
+            maxr = H[right]
+        else:
+            water = water + (maxr - H[right])
+        right = right - 1
+        
+print("Trapped Water = " + str(water))
