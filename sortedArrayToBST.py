ad112664a5384d51f5dcd308fb46c4851a68d50a
noopurkharche <32528824+noopurkharche@users.noreply.github.com>
2019-05-26 22:24:42 -0500

    Create sortedArrayToBST.py

---
+++ sortedArrayToBST.py
@@ -0,0 +1,22 @@
+# leetcode 108
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def sortedArrayToBST(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: TreeNode
+        """
+        if len(nums) == 0:
+            return None
+        n = len(nums)
+        mid = n // 2
+        node = TreeNode(nums[mid])
+        node.left = self.sortedArrayToBST(nums[:mid])
+        node.right = self.sortedArrayToBST(nums[mid+1:])
+        return node
